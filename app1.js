6d184d3a70468433c6b415f2b0bee07e65544414
Puneet <puneetv632@gmail.com>
2022-08-15 15:47:30 +0530

    add_phone-email_javascript

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
-let location_b = document.getElementById('location_b')
-let search_arrow = document.getElementById('search_arrow')
-let search_address = document.getElementById('search_address')
+let location_b = document.getElementById('location_b');
+let search_arrow = document.getElementById('search_arrow');
+let search_address = document.getElementById('search_address');
 
 search_address.addEventListener('focusin', ()=>{
     location_b.style.visibility = "visible";
@@ -12,3 +12,17 @@ search_address.addEventListener('focusout', ()=>{
     search_arrow.style.transform = "rotate(0deg)";
     location_b.style.opacity = 0;
 })
+
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let emails = document.getElementById('emails');
+let phones = document.getElementById('phones');
+let emails_phones = document.getElementById('emails_phones');
+phone.addEventListener('click', ()=>{
+    emails_phones.classList.add('show_more');
+    email.checked = false;
+});
+email.addEventListener('click', ()=>{
+    emails_phones.classList.remove('show_more');
+    phone.checked = false;
+});
